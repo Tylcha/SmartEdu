5444bdb25ddc62b16e72d7f554a6329c9dff9f9f
TALHA\talha <ttalhanazli@gmail.com>
2024-05-10 04:47:36 +0300

    +add particals, active link js

--- app.js
+++ app.js
@@ -10,7 +10,14 @@ app.set('view engine', 'ejs');
 
 //first page
 app.get('/', (req, res) => {
-    res.status(200).render('index');
+    res.status(200).render('index', {
+        page_name: 'index',
+    });
+});
+app.get('/about', (req, res) => {
+    res.status(200).render('about', {
+        page_name: 'about',
+    });
 });
 
 const PORT = 3000;
